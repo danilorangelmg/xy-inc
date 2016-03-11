3b8f1e680509ca3e9c66be75ff812dbc7fdb0689
Danilo <danilorangelmg@gmail.com>
2016-03-10 23:15:35 -0300

    Commit inicial

---
+++ xy-inc/Xy-inc/app/src/main/java/movies/com/br/xy_inc/connect/ConnectTask.java
@@ -0,0 +1,112 @@
+package movies.com.br.xy_inc.connect;
+
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.net.ssl.HttpsURLConnection;
+
+/**
+ * Created by danilo on 10/03/16.
+ */
+public class ConnectTask extends AsyncTask<Void, Void, Map<String, Object>> {
+
+    private Map<String, String> params = null;
+    private String baseUrl = "http://www.omdbapi.com/?";
+
+    public ConnectTask(Map<String, String> params) {
+        this.params = params;
+    }
+
+
+    @Override
+    protected Map<String, Object> doInBackground(Void... params) {
+        HttpURLConnection conn = null;
+        Map<String, Object> map = new HashMap<String, Object>();
+        try {
+
+            String response = "";
+            URL url = new URL(getUrl(this.params));
+            conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(10000);
+            conn.setConnectTimeout(15000);
+            conn.setRequestMethod("POST");
+            conn.setDoInput(true);
+            conn.setRequestProperty("Content-Type", "application/json");
+
+            OutputStream os = conn.getOutputStream();
+            BufferedWriter writer = new BufferedWriter(
+                    new OutputStreamWriter(os, "UTF-8"));
+            writer.flush();
+            writer.close();
+            os.close();
+            int responseCode=conn.getResponseCode();
+
+            if (responseCode == HttpsURLConnection.HTTP_OK) {
+                String line;
+                BufferedReader br=new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                while ((line=br.readLine()) != null) {
+                    response+=line;
+                }
+
+                Gson gson = new Gson();
+                map = (Map<String, Object>)gson.fromJson(response, map.getClass());
+            }
+            else {
+                //erro
+            }
+
+        } catch(Exception e) {
+            e.printStackTrace();
+        } finally {
+             if (conn != null) {
+                 conn.disconnect();
+             }
+        }
+        return map;
+    }
+
+    @Override
+    protected void onPostExecute(Map<String, Object> result) {
+        List<Map> movies = (List<Map>) result.get("Search");
+
+    }
+
+
+    private String getUrl(Map<String, String> params) throws UnsupportedEncodingException {
+
+        StringBuilder urlFinal = new StringBuilder();
+        urlFinal.append(baseUrl);
+        for (Map.Entry<String, String> param : params.entrySet()) {
+            urlFinal.append(param.getKey()).append("=").append(param.getValue()).append("&");
+        }
+
+        String url = urlFinal.toString();
+        url = url.substring(0, url.length()-1);
+
+        return url;
+
+    }
+
+
+
+
+}
---
+++ xy-inc/Xy-inc/app/src/main/java/movies/com/br/xy_inc/util/Const.java
@@ -0,0 +1,10 @@
+package movies.com.br.xy_inc.util;
+
+import android.os.Environment;
+
+/**
+ * Created by danilo on 10/03/16.
+ */
+public class Const {
+    public static final String IMAGE_PATH = Environment.getExternalStorageDirectory().getAbsolutePath();
+}
